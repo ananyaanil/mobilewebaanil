752adf492c626579736d80a1ab107611cba3e8dc
dcorey <dcorey.rasmussen@gmail.com>
2017-05-31 21:23:51 -0400

    updated host
    updated host

--- week2/config.php
+++ week2/config.php
@@ -1,6 +1,6 @@
 <?php
 // This is an example of config.php
-$dbhost = 'us-cdbr-azure-southcentral-f.cloudapp.net';
+$dbhost = 'us-cdbr-azure-central-a.cloudapp.net';
 $dbuser = 'b9250d1e5945d3';
 $dbpass = 'ca0a8ee6';
 $dbname = 'aanildb';
