6ffaff2c753af11e3ff89406f94c59c979b08f5b
dcorey <dcorey.rasmussen@gmail.com>
2017-05-31 21:18:10 -0400

    updated connection
    updated connection

--- week2/config.php
+++ week2/config.php
@@ -1,7 +1,7 @@
 <?php
 // This is an example of config.php
 $dbhost = 'us-cdbr-azure-southcentral-f.cloudapp.net';
-$dbuser = 'bbece113092b36';
-$dbpass = 'd9e55a1e';
-$dbname = 'db-83e13e57-9556';
+$dbuser = 'b9250d1e5945d3';
+$dbpass = 'ca0a8ee6';
+$dbname = 'aanildb';
 ?>
--- week2/displayall.php
+++ week2/displayall.php
@@ -24,7 +24,7 @@
 					include 'opendb.php';
 
 					$sql= "SELECT id, fname, lname
-					FROM lecture1";
+					FROM customers";
 					$result = mysqli_query($conn, $sql);
 
 					if (mysqli_num_rows($result) > 0) {
